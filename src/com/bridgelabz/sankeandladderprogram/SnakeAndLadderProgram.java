638ba5b6d6d03eadb0a445b6fa1a9de14b0020a0
PREETI2096 <preetirg96@gmail.com>
2022-10-14 12:50:22 +0530

    [preeti]: added uc1 start game

---
+++ src/com/bridgelabz/sankeandladderprogram/SnakeAndLadderProgram.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.sankeandladderprogram;
+
+public class SnakeAndLadderProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to snake and ladder game program");
+		 int position = 0;
+		 System.out.println("position of single player is :" + position);
+
+	}
+
+}
